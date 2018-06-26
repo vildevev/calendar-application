0b5d35454fd621e67c5ef0e9af6762ed24d78e12
“Vilde <vilde@vevatne.no>
2018-06-26 12:47:57 -0700

    Change to postgres db

--- CalendarBackend/app/controllers/events_controller.rb
+++ CalendarBackend/app/controllers/events_controller.rb
@@ -11,6 +11,15 @@ class EventsController < ApplicationController
 		render json: @event, status: :created
 	end
 
+	def destroy 
+		@event = Event.where(id: params[:id]).first
+		if @event.destroy 
+			head(:ok)
+		else
+			head(:unprocessable_entity)
+		end
+	end 
+
 	private 
 		def event_params
 			params.require(:event).permit(:description)
--- CalendarBackend/db/schema.rb
+++ CalendarBackend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180626183748) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
